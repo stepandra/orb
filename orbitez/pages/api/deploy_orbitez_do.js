1249879569f01be4be6171d47d8e793d9e1eb290
Ihor Kubrysh <55651856+kubrysh@users.noreply.github.com>
2023-01-06 01:28:21 +0200

    Implement subdomain name generation

--- orbitez/pages/api/deploy_orbitez_do.js
+++ orbitez/pages/api/deploy_orbitez_do.js
@@ -1,15 +1,22 @@
 import { DigitalOceanAccount } from "../../services/digitalocean/digitalocean_account";
 import { Region } from "../../services/digitalocean/model/digitalocean";
+import sum from "hash-sum";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
     const { token, deployTezos, region, contractAddress, roomName } = req.body;
+
+    // Generating subdomain name for server's url
+    const hash = sum(Date.now().toString() + roomName);
+    const assignedSubdomain = `${roomName}-${hash}`;
+
     const doAccount = new DigitalOceanAccount("do_account", token, true);
-    doAccount.createServer({
+    await doAccount.createServer({
         region: new Region(region),
         name: "ORBITEZ_TEZ_NODE",
         shouldDeployNode: deployTezos,
         contractAddress,
         roomName,
+        assignedSubdomain,
     });
 
     res.send({ success: true });
