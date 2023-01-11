be1a7f62d396812a7122a9bff0994a5a7ff7b1f4
Ihor Kubrysh <55651856+kubrysh@users.noreply.github.com>
2023-01-11 05:05:02 +0200

    Improve <Planet /> loading, file structure, styles

--- orbitez/components/Planet/Planet.jsx
+++ orbitez/components/Planet/Planet.jsx
@@ -1,9 +1,24 @@
-import { PlanetGenerator }  from '@components/PlanetGenerator/PlanetGenerator';
+import React from "react";
+import { Shaders } from "./Shaders";
 
-export function Planet({ mintHash }) {
+export function Planet({ isPlanetReady }) {
     return (
         <div className="planet">
-            <PlanetGenerator mint_hash={mintHash} />
+            <Shaders />
+            {isPlanetReady || (
+                <p className="planet--loading-text">
+                    Loading NFT, please wait...
+                </p>
+            )}
+            <div
+                className={`planet-canvas-wrapper ${
+                    !isPlanetReady
+                        ? "planet__canvas-wrapper--not-displayed"
+                        : ""
+                }`.trimEnd()}
+            >
+                <canvas id="c" width="800" height="800"></canvas>
+            </div>
         </div>
-    )
+    );
 }
