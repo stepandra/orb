814feab296e4623ac922974ec135a00d11fc33df
Ihor Kubrysh <55651856+kubrysh@users.noreply.github.com>
2023-01-18 18:13:27 +0200

    Refactor useTezos hook

--- orbitez/hooks/useTezos.js
+++ orbitez/hooks/useTezos.js
@@ -1,89 +1,94 @@
-import { TezosToolkit } from "@taquito/taquito"
-import { NetworkType } from "@airgap/beacon-sdk"
-import { useState, useEffect } from 'react'
-import { useAppContext } from '../pages/_app'
-import { MichelCodecPacker } from '@taquito/taquito';
+import { TezosToolkit } from "@taquito/taquito";
+import { NetworkType } from "@airgap/beacon-sdk";
+import { useState, useEffect, useCallback } from "react";
+import { useAppContext } from "../pages/_app";
+import { MichelCodecPacker } from "@taquito/taquito";
 
 export function useTezos() {
-  const wallet = useAppContext()
-  const RPC_URL = 'https://rpc.tzkt.io/ghostnet';
-  const Tezos = new TezosToolkit(RPC_URL)
-  Tezos.setPackerProvider(new MichelCodecPacker())
-  Tezos.setWalletProvider(wallet)
+    const wallet = useAppContext();
+    const RPC_URL = "https://rpc.tzkt.io/ghostnet";
+    const Tezos = new TezosToolkit(RPC_URL);
+    Tezos.setPackerProvider(new MichelCodecPacker());
+    Tezos.setWalletProvider(wallet);
 
-  const [balance, setBalance] = useState(0)
-  const [address, setAddress] = useState('')
-  const [isAuthLoaded, setIsAuthLoaded] = useState(false);
+    const [balance, setBalance] = useState(0);
+    const [address, setAddress] = useState("");
+    const [isAuthLoaded, setIsAuthLoaded] = useState(false);
 
-  useEffect(() => {
-    const loadAuth = async () => {
-      try {
-        await connectionExistsCheck();
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setIsAuthLoaded(true)
-      };
-    }
+    useEffect(() => {
+        const loadAuth = async () => {
+            try {
+                await connectionExistsCheck();
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setIsAuthLoaded(true);
+            }
+        };
 
-    loadAuth();
-  }, []);
+        loadAuth();
+    }, []);
 
-  useEffect(() => {
-    const updateBalance = async () => {
-      if (address === '') return;
-      
-      const bal = await Tezos.rpc.getBalance(address)
-      setBalance(bal.toNumber() / 1000000)
-    }
+    useEffect(() => {
+        const updateBalance = async () => {
+            if (address === "") return;
 
-    updateBalance();
-  }, [address])
+            const bal = await Tezos.rpc.getBalance(address);
+            setBalance(bal.toNumber() / 1000000);
+        };
 
-  const connectionExistsCheck = async () => {
-    if (!wallet) return false
-    const activeAccount = await wallet.client.getActiveAccount()
-    if (activeAccount) {
-      console.log(`Already connected: ${activeAccount.address}`)
-      setAddress(activeAccount.address)
-      localStorage.setItem('tzAddress', address)
-      return true
-    }
-    return false
-  }
+        updateBalance();
+    }, [address]);
 
-  const connectWallet = async () => {
-    const connectionExists = await connectionExistsCheck()
-    if (!connectionExists) {
-      await wallet.requestPermissions({
-        network: {
-          type: NetworkType.GHOSTNET,
-          // type: NetworkType.MAINNET,
-          rpcUrl: RPC_URL,
-        },
-      })
-      const addr = await wallet.getPKH()
-      setAddress(addr)
-      localStorage.setItem('tzAddress', address)
-    } else {
-      alert(`Already connected`)
-    }
-  }
+    const connectionExistsCheck = useCallback(async () => {
+        if (!wallet) return false;
 
-  const disconnectWallet = async () => {
-    await wallet.clearActiveAccount()
-    setAddress('')
-    localStorage.removeItem('tzAddress')
-    alert('Disconnected.')
-  }
+        const activeAccount = await wallet.client.getActiveAccount();
 
-  return {
-    connectWallet,
-    disconnectWallet,
-    wallet,
-    Tezos,
-    address,
-    balance,
-    isAuthLoaded,
-  }
+        if (!activeAccount) return false;
+
+        const walletAddress = activeAccount.address;
+        console.log(`Already connected: ${walletAddress}`);
+        setAddress(walletAddress);
+        localStorage.setItem("tzAddress", walletAddress);
+
+        return true;
+    }, [wallet]);
+
+    const connectWallet = useCallback(async () => {
+        const connectionExists = await connectionExistsCheck();
+
+        if (connectionExists) {
+            alert(`Already connected`);
+            return;
+        };
+
+        await wallet.requestPermissions({
+            network: {
+                type: NetworkType.GHOSTNET,
+                // type: NetworkType.MAINNET,
+                rpcUrl: RPC_URL
+            }
+        });
+        const walletAddress = await wallet.getPKH();
+        setAddress(walletAddress);
+        localStorage.setItem("tzAddress", walletAddress);
+    }, [connectionExistsCheck, wallet]);
+
+    const disconnectWallet = useCallback(async () => {
+        await wallet.clearActiveAccount();
+        setAddress("");
+        localStorage.removeItem("tzAddress");
+        alert("Disconnected.");
+    }, [wallet]);
+
+    return {
+        connectWallet,
+        disconnectWallet,
+        wallet,
+        Tezos,
+        address,
+        balance,
+        isAuthLoaded
+    };
 }
